ab86d9615cf3290c83ead77954e7529001858084
harubaru <seizou.genkei@protonmail.com>
2022-03-04 23:02:52 -0700

    Fix twitter implementation.

--- eliza/client/bot.py
+++ eliza/client/bot.py
@@ -1,9 +1,12 @@
-from multiprocessing.connection import wait
 from shimeji import ChatBot
 from shimeji.preprocessor import ContextPreprocessor
 from shimeji.postprocessor import NewlinePrunerPostprocessor
 
+from core.logging import get_logger
+
 import tweepy
+
+logger = get_logger(__name__)
 
 class Bot:
     def __init__(self, name, model_provider, **kwargs):
@@ -40,19 +43,23 @@ class TwitterBot(Bot):
         # essentially a callback-like thing for TwitterClient
         super().__init__(name, model_provider,  **kwargs)
         self.kwargs = kwargs
+
         self.chatbot = ChatBot(
             name=self.name,
             model_provider=model_provider,
             preprocessors=[ContextPreprocessor()],
             postprocessors=[NewlinePrunerPostprocessor()]
         )
+
         self.client = tweepy.StreamingClient(
             bearer_token=self.kwargs['bearer_token']
         )
+
         self.auth = tweepy.OAuthHandler(
             self.kwargs['consumer_key'],
             self.kwargs['consumer_secret']
         )
+
         self.client_api = tweepy.Client(
             bearer_token=self.kwargs['bearer_token'],
             consumer_key=self.kwargs['consumer_key'],
@@ -61,6 +68,9 @@ class TwitterBot(Bot):
             access_token_secret=self.kwargs['access_token_secret'],
             wait_on_rate_limit=True
         )
+
+        self.expansions = ["referenced_tweets.id", "author_id"]
+        self.user_fields = ["name"]
 
         # add rule to check if user is talking to the bot
         self.client.add_rules(
@@ -83,27 +93,25 @@ class TwitterBot(Bot):
     # return a list of strings that are the conversational history of a tweet
     def get_conversation(self, id):
         conversation = []
-        expansions = ["referenced_tweets.id", "author_id"]
-        user_fields = ["name"]
-        tweet = self.client_api.get_tweet(id, expansions=expansions, user_fields=user_fields)
-        # tweet is a request object, which has the dict of the tweet
-        for user in tweet.data['includes']['users']:
-            if user['id'] == tweet.data['data']['author_id']:
-                author = user['name']
+        tweet = self.client_api.get_tweet(id, expansions=self.expansions, user_fields=self.user_fields)
         
-        conversation.append(f"{author}: {tweet.data['data']['text']}")
-        if 'referenced_tweets' in tweet.data['data']:
-            for tweet_id in tweet.data['data']['referenced_tweets']:
-                conversation.append(self.get_conversation(tweet_id['id']))
+        for user in tweet.includes['users']:
+            author = user['name']
+        
+        conversation.append(f"{author}: {tweet.data.text}")
+        if 'tweets' in tweet.includes:
+            for tweet_id in tweet.includes['tweets']:
+                # prepend parent tweet to conversation, since get_conversation returns an array, expand it
+                conversation.insert(0, self.get_conversation(tweet_id.id)[0])
         
         return conversation
     
     # on_tweet: check if tweet is in reply to bot, if it is, get the conversation and respond
     def on_tweet(self, tweet):
-        if tweet.in_reply_to_user_id:
-            conversation = self.get_conversation(tweet.id)
-            response = self.chatbot.respond(conversation, push_chain=False)
-            self.tweet(response, reply_to=tweet.id)
+        logger.info(f'Tweet received: {tweet.id}')
+        conversation = self.get_conversation(tweet.id)
+        response = self.chatbot.respond('\n'.join(conversation), push_chain=False)
+        self.tweet(response, reply_to=tweet.id)
 
     def run(self):
-        self.client.filter()
+        self.client.filter(expansions=self.expansions, user_fields=self.user_fields)
