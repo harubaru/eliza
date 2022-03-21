940b13c4e4121bbdb9e5280b92981d8d244ec050
harubaru <seizou.genkei@protonmail.com>
2022-03-21 13:49:53 -0700

    Add better logging

--- eliza/core/logging.py
+++ eliza/core/logging.py
@@ -1,9 +1,15 @@
+import os
+import time
 import logging
+
+# check if log directory exists
+if not os.path.exists('logs'):
+    os.makedirs('logs')
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s] %(levelname)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
-                    filename='log.txt',
+                    filename=f'logs/{time.strftime("%Y-%m-%d_%H-%M-%S")}.log',
                     filemode='w')
 
 def get_logger(name):
